da89c108c05315e09dec58026ac2ce5d1b6d3831
PrajwolNeupaneHerald <np03cs4a220031@heraldcollege.edu.np>
2022-12-06 23:06:57 +0545

    Final Changes to backend

--- index.js
+++ index.js
@@ -23,19 +23,7 @@ app.use("/skill", skillRoute);
 app.use("/message", messageRoute);
 app.use("/user", userRoute);
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
 
-    app.get("*", (req, res) => {
-
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-
-    })
-
-
-
-
-}
 
 
 app.listen(process.env.PORT || 8000, async () => {
