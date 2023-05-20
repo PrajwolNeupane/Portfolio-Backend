4185166de355e7d5ee88518e02f2b157b1faf676
PrajwolNeupane <prajwolneupane68@gmail.com>
2023-05-20 15:50:09 +0545

    Added Environment

--- model/index.js
+++ model/index.js
@@ -2,8 +2,22 @@ import mongoose from 'mongoose';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-export default mongoose.connect(`mongodb+srv://${process.env.DB_OWNER}:${encodeURIComponent(process.env.DB_PASSWORD)}@cluster0.0mhxwd2.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`).then(() => {
-    console.log(`Database connected`);
-}).catch((e) => {
-    console.log(e.message);
-})
+var connection;
+
+if(!process.env.MONGODB_URI){
+    console.log("Development")
+    connection = mongoose.connect(`mongodb+srv://${process.env.DB_OWNER}:${encodeURIComponent(process.env.DB_PASSWORD)}@cluster0.0mhxwd2.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`).then(() => {
+        console.log(`Database connected`);
+    }).catch((e) => {
+        console.log(e.message);
+    })
+}
+else{
+    console.log("Production")
+    connection = mongoose.connect(`${process.env.MONGODB_URI}`).then(() => {
+        console.log(`Database connected`);
+    }).catch((e) => {
+        console.log(e.message);
+    })
+}
+export default  connection;
